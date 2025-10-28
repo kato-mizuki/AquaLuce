c38104445e6ad1c060543bc52c5e026e86fe4450
kato-mizuki <mk1141629@gmail.com>
2025-10-28 09:49:38 +0900

    cart

--- resources/views/order/checkout.blade.php
+++ resources/views/order/checkout.blade.php
@@ -9,39 +9,43 @@
 
         <div class="mb-3">
             <label class="form-label">お名前</label>
-            <input type="text" name="name" class="form-control" required>
+            <input type="text" name="name" class="form-control">
         </div>
 
         <div class="mb-3">
             <label class="form-label">メールアドレス</label>
-            <input type="email" name="email" class="form-control" required>
+            <input type="email" name="email" class="form-control">
         </div>
 
         <div class="mb-3">
             <label class="form-label">電話番号</label>
-            <input type="text" name="phone" class="form-control" required>
+            <input type="text" name="phone" class="form-control">
         </div>
 
         <div class="mb-3">
             <label class="form-label">郵便番号</label>
             <div class="postal-wrapper">
-            <input type="text" id="postal_code" name="postal_code" class="form-control" required>
-            <button type="button" id="auto_fill" class="btn btn-primary">住所自動入力</button>
-        </div>
+                <input type="text" id="postal_code" name="postal_code" class="form-control">
+                <button type="button" id="auto_fill" class="btn-primary">住所自動入力</button>
+            </div>
         </div>
 
         <div class="mb-3">
             <label class="form-label">都道府県</label>
-            <input type="text" id="prefecture" name="prefecture" class="form-control" required>
+            <input type="text" id="prefecture" name="prefecture" class="form-control">
         </div>
 
         <div class="mb-3">
             <label class="form-label">市区町村</label>
-            <input type="text" id="city" name="city" class="form-control" required>
+            <input type="text" id="city" name="city" class="form-control">
         </div>
 
         <div class="mb-3">
-            <label class="form-label">番地・建物名</label>
+            <label class="form-label">町域・番地</label>
+            <input type="text" id="town" name="town" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">建物名</label>
             <input type="text" id="address" name="address" class="form-control">
         </div>
 
